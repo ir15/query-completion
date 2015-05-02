a3c0bbb839d2e39471eb97502b8d5a072caa2043
Jonas Stendahl <jonas.stenahl@outlook.com>
2015-05-02 13:53:38 +0200

    InField Autocompletion on titleText as well and general improvements

--- src/main/java/QueryCompletionRequestHandler.java
+++ src/main/java/QueryCompletionRequestHandler.java
@@ -11,6 +11,7 @@ import org.apache.solr.handler.RequestHandlerBase;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.SolrQueryResponse;
 import org.apache.solr.schema.IndexSchema;
+import org.apache.solr.schema.SchemaField;
 
 import java.io.IOException;
 import java.util.*;
@@ -21,19 +22,23 @@ public class QueryCompletionRequestHandler extends RequestHandlerBase {
 	@Override public void handleRequestBody(SolrQueryRequest solrQueryRequest, SolrQueryResponse solrQueryResponse)
 			throws Exception {
 
-		Collection<String> fields = solrQueryRequest.getSearcher().getFieldNames();
+		Collection<String> fieldNames = solrQueryRequest.getSearcher().getFieldNames();
 		IndexSchema index = solrQueryRequest.getSearcher().getSchema();
+		Map<String, SchemaField> schemaFields = index.getFields();
+
+		List<String> fields = getStoredFields(fieldNames, schemaFields);
+
 		SolrParams params = solrQueryRequest.getParams();
 
 		ArrayList<SimpleOrderedMap<String>> results = new ArrayList<SimpleOrderedMap<String>>();
 		String q = params.get(CommonParams.Q);
-		if (isInFieldAutoComplete(q)){
-			results = inFieldAutoCompletion(q);
-		}
-		else if (q.endsWith(" ")){
+		if (linkWords(q)){
 			results.add(wrappInMap(q + "AND "));
 			results.add(wrappInMap(q + "OR "));
 			results.add(wrappInMap(q + "NOT "));
+		}
+		else if (isInFieldAutoComplete(q)){
+			results = inFieldAutoCompletion(q);
 		}
 		else {
 			String[] twoPartString = getStringParts(q);
@@ -42,31 +47,53 @@ public class QueryCompletionRequestHandler extends RequestHandlerBase {
 		solrQueryResponse.add("auto_complete", results);
 	}
 
+	private boolean linkWords(String q){
+		if (q.endsWith(" ")){
+			if (q.endsWith("AND ") || q.endsWith("OR ") || q.endsWith("NOT ")){
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private List<String> getStoredFields(Collection<String> fieldNames, Map<String, SchemaField> schemaFields) {
+		List<String> fields = new ArrayList<String>(fieldNames.size());
+		for(String fieldName : fieldNames){
+			if (schemaFields.get(fieldName).stored()){
+				fields.add(fieldName);
+			}
+		}
+		return fields;
+	}
+
 	private ArrayList<SimpleOrderedMap<String>> inFieldAutoCompletion(String q) throws SolrServerException, IOException {
 		ArrayList<SimpleOrderedMap<String>> results = new ArrayList<SimpleOrderedMap<String>>();
 		if (q.endsWith(")")){
 			q = q.substring(0, q.length()-1);
 		}
 		if (!q.endsWith("(")) {
+			String field = q.substring(0, q.lastIndexOf(':'));
+			field = field.substring(field.lastIndexOf(' ') + 1);
 			String fieldSearch = q.substring(q.lastIndexOf('(') + 1);
 			q = q.substring(0, q.lastIndexOf('('));
 			String[] twoPartString = getStringParts(fieldSearch);
-			QueryResponse suggested = getSuggestions(twoPartString[1]);
+			QueryResponse suggested = getSuggestions(field, twoPartString[1]);
 
 			List<SpellCheckResponse.Suggestion> suggestions = suggested.getSpellCheckResponse().getSuggestions();
 			for (SpellCheckResponse.Suggestion suggestion : suggestions) {
 				for (String alternative : suggestion.getAlternatives()) {
-					results.add(wrappInMap(q + "(" + (twoPartString[0] + " " + alternative).trim() + ")"));
+					results.add(wrappInMap(q + "(" + (twoPartString[0] + " " + alternative).trim() + " "));
 				}
 			}
 		}
 		return results;
 	}
 
-	private QueryResponse getSuggestions(String s) throws SolrServerException, IOException {
+	private QueryResponse getSuggestions(String field, String s) throws SolrServerException, IOException {
 		SolrClient solr = new HttpSolrClient("http://localhost:8983/solr/Test");
 		SolrQuery parameters = new SolrQuery();
-		parameters.set("qt", "/suggest");
+		parameters.set("qt", "/suggest_" + field);
 		parameters.set("q", s);
 		return solr.query(parameters);
 	}
@@ -75,7 +102,7 @@ public class QueryCompletionRequestHandler extends RequestHandlerBase {
 		ArrayList<SimpleOrderedMap<String>> results = new ArrayList<SimpleOrderedMap<String>>();
 		for (String field : fields){
 			if ((field.length() >= twoPartString[1].length()) && (field.substring(0, twoPartString[1].length()).equals(twoPartString[1]))){
-				results.add(wrappInMap(twoPartString[0] + " " + field + ":()"));
+				results.add(wrappInMap(twoPartString[0] + " " + field + ":("));
 				addRangeIfRangeField(index, twoPartString, results, field);
 			}
 		}
